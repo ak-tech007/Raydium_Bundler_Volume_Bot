004b1c701c4260bb1249c2bdb8b591b784f4b1b4
ak-tech007 <lakatanana1130@gmail.com>
2025-03-22 08:02:25 +0200

    updated withdraw mechanism

--- src/utils/pool.tsx
+++ src/utils/pool.tsx
@@ -25,6 +25,7 @@ const jitoUrl = process.env.NEXT_PUBLIC_JITO_URL;
 import { getDefaultStore } from "jotai";
 import { marketIdAtom, vaultsAtom } from "../state/atoms";
 import { get_market } from "./market";
+import { get_market_keys } from "@/lib/getMarketKeys";
 const store = getDefaultStore();
 
 dotenv.config();
@@ -450,69 +451,67 @@ export const initializeAndSwap = async (InitialAndSwapDetails: {
     });
     store.set(marketIdAtom, market_id);
 
-    return signature_swap3;
+    const jitoTipAddress = await getJitoTipAccount();
 
-    // const jitoTipAddress = await getJitoTipAccount();
+    const tipIx = SystemProgram.transfer({
+      fromPubkey: wallet.publicKey,
+      toPubkey: jitoTipAddress,
+      lamports: jito_fee, // tip amount
+    });
 
-    // const tipIx = SystemProgram.transfer({
-    //   fromPubkey: wallet.publicKey,
-    //   toPubkey: jitoTipAddress,
-    //   lamports: jito_fee, // tip amount
-    // });
+    const transaction_jitoTip = new Transaction().add(tipIx);
+    transaction_jitoTip.recentBlockhash = (
+      await connection.getLatestBlockhash()
+    ).blockhash;
+    transaction_jitoTip.feePayer = wallet.publicKey;
+    const transaction_jitoTip_ =
+      await wallet.signTransaction(transaction_jitoTip);
 
-    // const transaction_jitoTip = new Transaction().add(tipIx);
-    // transaction_jitoTip.recentBlockhash = (
-    //   await connection.getLatestBlockhash()
-    // ).blockhash;
-    // transaction_jitoTip.feePayer = wallet.publicKey;
-    // const transaction_jitoTip_ =
-    //   await wallet.signTransaction(transaction_jitoTip);
+    const bunldeSentResult = await queryJitoBundles("sendBundle", [
+      bs58.encode(signedTransaction_init.serialize()),
+      bs58.encode(transaction_swap1.serialize()),
+      bs58.encode(transaction_swap2.serialize()),
+      bs58.encode(transaction_swap3.serialize()),
+      bs58.encode(transaction_jitoTip_.serialize()),
+    ]);
 
-    // const bunldeSentResult = await queryJitoBundles("sendBundle", [
-    //   bs58.encode(signedTransaction_init.serialize()),
-    //   bs58.encode(transaction_swap1.serialize()),
-    //   bs58.encode(transaction_swap2.serialize()),
-    //   bs58.encode(transaction_swap3.serialize()),
-    //   bs58.encode(transaction_jitoTip_.serialize()),
-    // ]);
+    console.log(`✅ Bundle sent: ${bunldeSentResult?.result}`);
 
-    // console.log(`✅ Bundle sent: ${bunldeSentResult?.result}`);
+    let retryCount = 0;
+    const timeBetweenRetries = 5000;
+    const maxRetries = 30;
 
-    // let retryCount = 0;
-    // const timeBetweenRetries = 5000;
-    // const maxRetries = 20;
+    do {
+      const inflightBundleStatus = await queryInflightBundleStatuses(
+        "getInflightBundleStatuses",
+        [bunldeSentResult?.result]
+      );
 
-    // do {
-    //   const inflightBundleStatus = await queryInflightBundleStatuses(
-    //     "getInflightBundleStatuses",
-    //     [bunldeSentResult?.result]
-    //   );
+      const bundleStatus = inflightBundleStatus?.result.value?.[0].status;
 
-    //   const bundleStatus = inflightBundleStatus?.result.value?.[0].status;
+      if (bundleStatus === "Failed") {
+        console.log("❌ JITO bundle failed");
+        return "Failed";
+      }
 
-    //   if (bundleStatus === "Failed") {
-    //     console.log("❌ JITO bundle failed");
-    //     return "Failed";
-    //   }
+      if (bundleStatus === "Landed") {
+        console.log("✅ JITO bundle landed");
+        const bundle = await queryBundleStatuses("getBundleStatuses", [
+          bunldeSentResult?.result,
+        ]);
+        console.log(
+          `📝 Transactions: ${bundle?.result.value?.[0].transactions}`
+        );
 
-    //   if (bundleStatus === "Landed") {
-    //     console.log("✅ JITO bundle landed");
-    //     const bundle = await queryBundleStatuses("getBundleStatuses", [
-    //       bunldeSentResult?.result,
-    //     ]);
-    //     console.log(
-    //       `📝 Transactions: ${bundle?.result.value?.[0].transactions}`
-    //     );
+        return bundle?.result.value?.[0].transactions[0];
+      }
 
-    //     return bundle?.result.value?.[0].transactions;
-    //   }
+      console.log(`🔄 JITO bundle status: ${bundleStatus}`);
+      retryCount++;
+      await new Promise((resolve) => setTimeout(resolve, timeBetweenRetries));
+    } while (retryCount < maxRetries);
 
-    //   console.log(`🔄 JITO bundle status: ${bundleStatus}`);
-    //   retryCount++;
-    //   await new Promise((resolve) => setTimeout(resolve, timeBetweenRetries));
-    // } while (retryCount < maxRetries);
-
-    // return "Failed";
+    return "Failed";
   } catch (error) {
     if (error instanceof SendTransactionError) {
       console.error("Transaction failed:", error.message);
@@ -522,6 +521,176 @@ export const initializeAndSwap = async (InitialAndSwapDetails: {
     throw error;
   }
 };
+
+export async function withdraw_pool(withdrawDetails: {
+  percentage: number;
+  mint: string;
+  market_id: string;
+  wallet: any;
+}) {
+  const { percentage, mint, market_id, wallet } = withdrawDetails;
+  const provider = new anchor.AnchorProvider(
+    connection,
+    wallet,
+    anchor.AnchorProvider.defaultOptions()
+  );
+  const contractProgramId = new PublicKey(
+    "DdHFxFR8mR8yVWwYhK9PHPTHTBJf1xQHTRMhEXGoJC1f"
+  );
+  const program = new Program(idl as any, contractProgramId, provider);
+
+  const RAYDIUM_PROGRAM_ID = new PublicKey(
+    "HWy1jotHpo6UqeQxx49dpYYdQB8wj9Qk9MdxwjLvDHB8"
+  ); // Replace with Raydium program ID
+  const OPENBOOK_PROGRAM_ID = new PublicKey(
+    "EoTcMgcDRTJVZDMZWBoU6rhYHZfkNTVEAfz3uUJRcYGj"
+  ); // Replace with OpenBook program ID
+  const MARKET_KEY = new PublicKey(market_id); // Replace with OpenBook market key
+  const COIN_MINT = new PublicKey(mint); // Replace with coin mint
+  const PC_MINT = new PublicKey("So11111111111111111111111111111111111111112");
+
+  const [ammPda, ammBump] = PublicKey.findProgramAddressSync(
+    [
+      RAYDIUM_PROGRAM_ID.toBuffer(),
+      MARKET_KEY.toBuffer(),
+      Buffer.from("amm_associated_seed"),
+    ],
+    RAYDIUM_PROGRAM_ID
+  );
+  const [ammAuthorityPda, ammAuthorityBump] = PublicKey.findProgramAddressSync(
+    [Buffer.from("amm authority")],
+    RAYDIUM_PROGRAM_ID
+  );
+  const [ammOpenOrdersPda, ammOpenOrdersbump] =
+    PublicKey.findProgramAddressSync(
+      [
+        RAYDIUM_PROGRAM_ID.toBuffer(),
+        MARKET_KEY.toBuffer(),
+        Buffer.from("open_order_associated_seed"),
+      ],
+      RAYDIUM_PROGRAM_ID
+    );
+  const [ammLpMintPda, ammLpMintbump] = PublicKey.findProgramAddressSync(
+    [
+      RAYDIUM_PROGRAM_ID.toBuffer(),
+      MARKET_KEY.toBuffer(),
+      Buffer.from("lp_mint_associated_seed"),
+    ],
+    RAYDIUM_PROGRAM_ID
+  );
+  const [ammCoinVaultPda, ammCoinVaultbump] = PublicKey.findProgramAddressSync(
+    [
+      RAYDIUM_PROGRAM_ID.toBuffer(),
+      MARKET_KEY.toBuffer(),
+      Buffer.from("coin_vault_associated_seed"),
+    ],
+    RAYDIUM_PROGRAM_ID
+  );
+  const [ammPcVaultPda, ammPcVaultbump] = PublicKey.findProgramAddressSync(
+    [
+      RAYDIUM_PROGRAM_ID.toBuffer(),
+      MARKET_KEY.toBuffer(),
+      Buffer.from("pc_vault_associated_seed"),
+    ],
+    RAYDIUM_PROGRAM_ID
+  );
+  const [ammTargetOrdersPda, ammTargetOrdersbump] =
+    PublicKey.findProgramAddressSync(
+      [
+        RAYDIUM_PROGRAM_ID.toBuffer(),
+        MARKET_KEY.toBuffer(),
+        Buffer.from("target_associated_seed"),
+      ],
+      RAYDIUM_PROGRAM_ID
+    );
+  const user_token_coin = await getAssociatedTokenAddress(
+    COIN_MINT,
+    wallet.publicKey,
+    false,
+    TOKEN_PROGRAM_ID,
+    ASSOCIATED_TOKEN_PROGRAM_ID
+  );
+  const user_token_pc = await getAssociatedTokenAddress(
+    PC_MINT,
+    wallet.publicKey,
+    false,
+    TOKEN_PROGRAM_ID,
+    ASSOCIATED_TOKEN_PROGRAM_ID
+  );
+  const user_token_lp = await getAssociatedTokenAddress(
+    ammLpMintPda,
+    wallet.publicKey,
+    false,
+    TOKEN_PROGRAM_ID,
+    ASSOCIATED_TOKEN_PROGRAM_ID
+  );
+  const tokenProgram = TOKEN_PROGRAM_ID;
+
+  const market_keys = await get_market(market_id);
+
+  const MARKET_BIDS_KEY = new PublicKey(market_keys.marketBids);
+  const MARKET_ASKS_KEY = new PublicKey(market_keys.marketAsks);
+  const MARKET_EVENT_QUEUE_KEY = new PublicKey(market_keys.marketEventQueue);
+  const MARKET_COIN_VAULT_KEY = new PublicKey(market_keys.marketCoinVault);
+  const MARKET_PC_VAULT_KEY = new PublicKey(market_keys.marketPcVault);
+  const MARKET_VAULT_SIGNER_KEY = new PublicKey(market_keys.marketVaultSigner);
+
+  const LP_balance = await getTokenBalance(wallet.publicKey, ammLpMintPda);
+
+  try {
+    const transaction_withdraw = new Transaction();
+    transaction_withdraw.add(
+      await program.methods
+        .withdraw(
+          new anchor.BN(BigInt(Math.floor((LP_balance * percentage) / 100))),
+          new anchor.BN(BigInt(0)),
+          new anchor.BN(BigInt(0))
+        )
+        .accounts({
+          ammProgram: RAYDIUM_PROGRAM_ID,
+          amm: ammPda,
+          ammAuthority: ammAuthorityPda,
+          ammOpenOrders: ammOpenOrdersPda,
+          ammTargetOrders: ammTargetOrdersPda,
+          ammLpMint: ammLpMintPda,
+          ammCoinVault: ammCoinVaultPda,
+          ammPcVault: ammPcVaultPda,
+          marketProgram: OPENBOOK_PROGRAM_ID,
+          market: MARKET_KEY,
+          marketCoinVault: MARKET_COIN_VAULT_KEY,
+          marketPcVault: MARKET_PC_VAULT_KEY,
+          marketVaultSigner: MARKET_VAULT_SIGNER_KEY,
+          marketEventQueue: MARKET_EVENT_QUEUE_KEY,
+          marketBids: MARKET_BIDS_KEY,
+          marketAsks: MARKET_ASKS_KEY,
+          userLpTokenAccount: user_token_lp,
+          userCoinAccount: user_token_coin,
+          userPcAccount: user_token_pc,
+          userWallet: wallet.publicKey,
+          splToken: tokenProgram,
+        })
+        .instruction()
+    );
+
+    transaction_withdraw.recentBlockhash = (
+      await connection.getLatestBlockhash()
+    ).blockhash;
+    transaction_withdraw.feePayer = wallet.publicKey;
+
+    const signature_withdraw =
+      await wallet.signTransaction(transaction_withdraw);
+
+    const signature = await connection.sendRawTransaction(
+      signature_withdraw.serialize()
+    );
+
+    console.log("Transaction sent, awaiting confirmation...");
+    await connection.confirmTransaction(signature);
+    console.log("Transaction confirmed:", signature_withdraw);
+  } catch (error) {
+    console.error("Error during withdrawal transaction:", error);
+  }
+}
 
 async function queryJitoBundles(method: string, params: any[]) {
   try {
